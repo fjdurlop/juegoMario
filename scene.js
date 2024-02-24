4cfa50175468e459a7f1752d6b8748bc527d7886
TSWS <xinlei0116@gmail.com>
2024-02-24 18:24:43 +0100

    add status text(not finish yet)

--- scene.js
+++ scene.js
@@ -32,18 +32,46 @@ Scene.prototype.update = function (deltaTime) {
 		this.bubbleActive = false;
 }
 
+async function drawStatusText(context) {
+	
+	// Load and set the font
+	const pixelFont = new FontFace('PublicPixel', 'url(font/PublicPixel.ttf)');
+	document.fonts.add(pixelFont);
+	await pixelFont.load();
+
+	context.font = "900 15px PublicPixel";
+	context.fillStyle = 'white';
+	
+	// Draw text on the canvas
+	var text = 'MARIO';
+	//var textSize = context.mesureText(text);
+	context.fillText(text, 20,30);
+	context.fillText('WORLD', 280, 30);
+	context.fillText('TIME', 410, 30);
+
+	context.fillText('000000', 20, 45);
+	context.fillText('x 00', 150, 45);
+	context.fillText('1-1', 300, 45);
+	context.fillText((400-Math.floor(this.currentTime / 1000))+' ', 410, 45);
+	//TODO: when times up, do something
+}
+
 Scene.prototype.draw = function () {
 	// Get canvas object, then its context
 	var canvas = document.getElementById("game-layer");
 	var context = canvas.getContext("2d");
 
 	// Clear background
-	context.fillStyle = "rgb(224, 224, 240)";
+	context.fillStyle = "rgb(90, 150, 240)";
 	context.fillRect(0, 0, canvas.width, canvas.height);
 
 	// Draw tilemap
 	this.map.draw();
 
+	drawStatusText();
+
+
+	
 	// Draw entities
 	if (this.bubbleActive)
 		this.bubble.draw();
